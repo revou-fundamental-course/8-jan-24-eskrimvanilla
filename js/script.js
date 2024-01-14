9f2a6bd3cda5a5183a5ccd5e9983dc18d320e4be
unknown <ahmadyudhaaditya@gmail.com>
2024-01-15 00:17:48 +0700

    final

--- js/script.js
+++ js/script.js
@@ -10,5 +10,20 @@ document.getElementById('calculator-form').addEventListener('submit', function(e
 
     //put in html by assigning it to luas-persegi id using innerHTML
     document.getElementById('luas-persegi').innerHTML = 
-    'L = S x S' + '<br>' + 'L = ' + sisi + 'x' + sisi + '<br>' + 'L = ' + luasPersegi
+    'L = S x S <br> L = ' + sisi + 'x' + sisi + '<br> L = ' + luasPersegi
+});
+
+//get html element from form id, listens to submit, and runs the script
+document.getElementById('form').addEventListener('submit', function(event) {
+    //prevents the page from refreshing :)
+    event.preventDefault();
+
+    //get side from html id and change it to a floating point
+    var sisi = parseFloat(document.getElementById('side').value);
+    //formula for calculating the circumference of a square
+    var kelPersegi = '4' * sisi;
+
+    //put in html by assigning it to keliling id using innerHTML
+    document.getElementById('keliling').innerHTML = 
+    'K = 4 x S <br> K = 4 x' + sisi + '<br> K = ' + kelPersegi
 });
